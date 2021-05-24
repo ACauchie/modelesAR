a97b36fd14208e3c1c8142808636e9e3c731a3a6
ACauchie <84713496+ACauchie@users.noreply.github.com>
2021-05-24 13:52:42 +0200

    add demo AR site

---
+++ site_ar/switch.js
@@ -0,0 +1,26 @@
+	var el=document.getElementById("security");
+	var el1=document.getElementById("vr");
+	var el2=document.getElementById("compo");
+	var el3=document.getElementById("pix");
+
+	//var scene=document.getElementById("secuDiv");
+	//var scene1=document.getElementById("vrDiv");
+	//var scene2=document.getElementById("compoDiv");
+	//var scene3=document.getElementById("pixDiv");
+
+
+function launchSecu(){
+	window.location.href = 'secu.html'
+}
+
+function launchVR(){
+	window.location.href = 'index.html'
+}
+
+function launchCompo(){
+	window.location.href = 'compo.html'
+}
+
+function launchPix(){
+	window.location.href = 'pix.html'
+}
